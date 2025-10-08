f18a0e55701f33c3619af8b1462330630de62a5c
staasan <staachan@yandex.ru>
2025-10-08 13:33:35 +0300

    Перевод файла menu.py на русский + небольшие поправки

--- menu.py
+++ menu.py
@@ -7,66 +7,75 @@ from tkinter import filedialog
 
 def key_menu():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("Select file for key generation")
+    print("Выберите файл")
     selected_file_name = filedialog.askopenfilename()
-    print("Select which name key file should have and its save path")
+    print("Куда сохранить ключ?")
     key_file_name = filedialog.asksaveasfilename()
-    codes_amount = int(input("How many codewords key file should have? "))
-    code_bytes = int(input("How many bytes codeword in key should have? "))
-    print("File reading...")
+    while True:
+        codes_amount = int(input("Сколько кодовых слов должно быть в ключе?"))
+        code_bytes = int(input("Сколько байт должно содержать одно кодовое слово?"))
+        if 16**code_bytes < codes_amount:
+            print(f"Невозможно сгенерировать {codes_amount} ключей из {code_byte} байт/n Выберите другие значения.")
+    print("Чтение файла...")
     symbols_frequency = read.symbol_frequency(selected_file_name,codes_amount)
-    print("Key generation...")
+    print("Генерация ключа...")
     key_file_name = key.generate_key_file(symbols_frequency,code_bytes,key_file_name)
-    print(f"Key file {key_file_name} was generated!")
-    print("Press Enter to continue...")
+    print(f"Файл ключа {key_file_name} сгенерирован")
+    print("Нажмите Enter для продолжения...")
     input()
     os.system('cls' if os.name == 'nt' else 'clear')
     main_menu()
 
 
 def enc_menu():
-    os.system('cls')
-    print("Select file to encrypt")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Выберите файл")
     selected_file_name = filedialog.askopenfilename()
-    print("Select key file")
+    print("Выберите ключ")
     key_file_name = ""
     while ".pymo" not in key_file_name:
         key_file_name = filedialog.askopenfilename()
         if "pymo" in key_file_name: break
-        else: print("Selected file is not a key file")
-    print("Encrypting...")
+        else: print("Выбранный файл не является ключом")
+    print("Шифрование файла...")
     encrypted_file_name = encrypt.encrypt(selected_file_name,key_file_name)
-    print(f"File was encrypted! {encrypted_file_name}")
-    print("Press Enter to continue...")
+    print(f"Файл зашифрован. {encrypted_file_name}")
+    print("Нажмите Enter для продолжения...")
     input()
-    os.system('cls'*(os.name == "nt")+"clear"*(os.name == "posix"))
+    os.system('cls' if os.name == 'nt' else 'clear')
     main_menu()
 
 def dec_menu():
-    os.system('cls')
-    print("Selected encrypted file")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Выберите файл")
     selected_file_name = filedialog.askopenfilename()
-    print("Select key file")
-    key_file_name = ""
-    while ".pymo" not in key_file_name:
+    print("Выберите ключ")
+    while True:
         key_file_name = filedialog.askopenfilename()
         if ".pymo" in key_file_name:
             break
-        else:
-            print("Selected file is not a key file")
-    print("Decrypting...")
-    decrypted_file_name = decrypt.decrypt(selected_file_name, key_file_name)
+        print("Selected file is not a key file")
+    print("Расшифрование файла...")
+    try:
+        decrypted_file_name = decrypt.decrypt(selected_file_name, key_file_name)
+    except KeyError:
+        print("Неверный ключ")
+        print("Нажмите Enter для продолжения...")
+        input()
+        os.system('cls' if os.name == 'nt' else 'clear')
+        main_menu()
+        
     print(f"File {decrypted_file_name} was decrypted")
-    print("Press Enter to continue...")
+    print("Нажмите Enter для продолжения...")
     input()
-    os.system('cls'*(os.name == "nt")+"clear"*(os.name == "posix"))
+    os.system('cls' if os.name == 'nt' else 'clear')
     main_menu()
 
 
 def main_menu():
-    print("Pymophonic ver. 1.0.0. Execute main.py -h to read manual")
+    print("Pymophonic. Запустите файл main.py с аргументом -h чтобы прочитать руководство")
     while True:
-        pressed_key = input("\n[1] Key generation \n[2] File encryption \n[3] File decryption\n[4] Quit\n")[0]
+        pressed_key = input("\n[1] Генерация ключа \n[2] Шифрование файла \n[3] Дешифрование файла\n[4] Выход\n")[0]
         match pressed_key:
             case "1":
                 key_menu()
@@ -77,5 +86,6 @@ def main_menu():
             case "4":
                 quit()
             case _:
-                os.system('cls'*(os.name == "nt")+"clear"*(os.name == "posix"))
-                print("Invalid input")
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print("Неверный ввод")
+
